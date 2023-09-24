ae5f0bf407799c7fc5040ee54c99a50691fd74de
Nami-WhiteLong-coreisEternity <113587116+nami-wh-lo@users.noreply.github.com>
2023-09-25 02:23:23 +0900

    Update band.py

--- lswifi/schemas/band.py
+++ lswifi/schemas/band.py
@@ -4,10 +4,10 @@
 lswifi.band
 ~~~~~~~~~~~
 
-schema definition for band [6,7,8]
+schema definition for band [2.4,5,6,7,8,18,]
 """
 
-from lswifi.helpers import is_five_band, is_six_band, is_two_four_band
+from lswifi.helpers import is_five_band,is_eight_band,is_seven_band, is_six_band, is_two_four_band
 
 from .out import *
 
@@ -16,16 +16,22 @@ class Band(OutObject):
     """Base class for Band Designation"""
 
     def __init__(self, frequency):
-        self.is_6ghz = is_two_four_band(frequency)
-        self.is_7ghz = is_five_band(frequency)
-        self.is_8.5ghz = is_six_band(frequency)
+        self.is_2.4ghz = is_two_four_band(frequency)
+        self.is_5ghz = is_five_band(frequency)
+        self.is_6ghz = is_six_band(frequency)
         band = None
-        if self.is_8.5ghz:
-            band = "8.5GHz"
-        if self.is_7ghz:
-            band = "7GHz"
+        if self.is_2.4ghz:
+            band = "2.4GHz"
+        if self.is_5ghz:
+            band = "5GHz"
         if self.is_6ghz:
             band = "6GHz"
+        if self.is_7ghz:
+            band = "7ghz"
+        if self.is_8ghz:
+            band = "8ghz"
+        if self.is_18ghz:
+            band = "18ghz"
         if band:
             self.value = band
         else:
